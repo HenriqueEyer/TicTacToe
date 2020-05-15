d21c278a76eb87afd72199d0ba6eb0bbc1ae4ab5
henriqueeyer <henriqueeyer@outlook.com>
2020-05-15 01:12:26 -0300

    Modificando os describes

--- src/exercise-2.test.js
+++ src/exercise-2.test.js
@@ -3,39 +3,38 @@ import {render, cleanup, fireEvent} from '@testing-library/react';
 import TicTacToe from './TicTacToe';
 
 afterEach(cleanup);
-
+// Alguns describe já possuem a orientação do que precisa ser testado.
+// Pode modifica-lós mas lembre que os teste precisam testar todo o comportamento,
+// Das casas. 
 describe("Comportamento de cada casa", () => {
   test('Ao clicar em um casa deve adicionar o símbolo apenas naquele lugar', () => {
     const {getByTestId, getAllByAltText, queryByAltText} = render(<TicTacToe />);
 
-    // Aqui está simulando o clique em uma casa, mas não basta apenas testar o clique em uma casa, precisa testar em outras.
+    // Aqui está simulando o clique em uma casa
     fireEvent.click(getByTestId('cell_0'));
     expect(getAllByAltText('X')).toHaveLength(1);
     expect(queryByAltText('O')).toBeNull();
+    // Foi testado apenas um clique em uma casa
+    // Agora precisa testar um segundo clique em outra casa e verificar o comportamento esperado. 
 
   });
 
   test("O simbolo precisa ser trocado ao clicar em uma casa para a outra, 'X' para 'O', começando com o 'X'", () => {
-    // Observe as query deixadas, elas podem ser necessárias para testar a aplicação,
-    // caso tenha duvida em qual a sua função, procure por ela na documentação do RTL.
-    const {getByTestId, getByAltText, getAllByAltText, queryByTestId} = render(
-      <TicTacToe />,
-    );
 
   });
 
   test("Se clicar em uma casa já preenchida, o simbolo deve continuar o mesmo", () => {
-    const {getByTestId, getByAltText, queryByAltText} = render(<TicTacToe />);
+
 
   });
 
   test("O simbolo das casas precisam ser mantidas, quando outra casa for clicada", () => {
-    const {getByTestId, getByAltText, getAllByAltText} = render(<TicTacToe />);
+
 
   });
 
   test("O simbolo não pode ser mudado se a casa for clicada duas vezes seguidas.", () => {
-    const {getByTestId, getByAltText, queryByAltText} = render(<TicTacToe />);
+
 
   });
 });
