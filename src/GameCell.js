eec7f662d76f1c406a770dc345c0368c6721ba4f
henriqueeyer <henriqueeyer@outlook.com>
2020-05-15 01:58:27 -0300

    Adicionando observaçoes

--- src/GameCell.js
+++ src/GameCell.js
@@ -4,11 +4,15 @@ import './GameCell.css';
 import xImage from './x.png';
 import oImage from './o.svg';
 
+// Não mude a propriedade da div data-testid de cada casa
+// ele será utilizado para o terceiro exercício!
+// Use-o para selecionar uma casa especifica nas horas dos testes.
+
 class GameCell extends React.Component {
   render() {
     const { id } = this.props;
     return (
-      <div data-testid={`cell_${id}`} className="game-cell" />
+      <div data-testid={`cell_${id}`} className="game-cell" /> 
     );
   }
 }
